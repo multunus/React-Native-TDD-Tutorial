23f4bfefb36e89f9c5eee722236c462bd84be4fe
Peter Lazar <peterlazar1993@gmail.com>
2016-02-17 16:51:59 +0530

    add linting
    lining using eslint.org

--- index.android.js
+++ index.android.js
@@ -2,13 +2,13 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
+
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
 class ReactNativeBoilerplate extends Component {
