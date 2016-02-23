4e78542e26fb87ae5c2d30f2e7d61b10fd554334
Peter Lazar <peterlazar1993@gmail.com>
2016-02-23 17:51:50 +0530

    setup redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,14 +3,24 @@ import { Map } from 'immutable';
 import { applyMiddleware, createStore } from 'redux';
 import reducers from './../modules/reducers';
 import sagaMiddleware from './../modules/sagas';
+import { Iterable } from 'immutable';
 
 const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
+  const stateTransformer = (state) => {
+    if (Iterable.isIterable(state)) return state.toJS();
+    else return state;
+  };
+  const actionTransformer = (action) => {
+    if (Iterable.isIterable(action.payload)) return { type: action.type, payload: action.payload.toJS() };
+    else return action;
+  };
   const createLogger = require('redux-logger');
-  const logger = createLogger({ collapsed: true });
+  const logger = createLogger({ collapsed: true, stateTransformer, actionTransformer });
   middlewares.push(logger);
 }
+
 
 export default function configureStore() {
   return createStore(reducers, Map(), applyMiddleware(...middlewares));
