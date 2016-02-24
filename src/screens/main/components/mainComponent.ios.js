4fb9ba27f05da77184313bdb699e9fa223499a5d
Peter Lazar <peterlazar1993@gmail.com>
2016-02-24 16:10:27 +0530

    use common styling

--- src/screens/main/components/mainComponent.ios.js
+++ src/screens/main/components/mainComponent.ios.js
@@ -1,28 +1,7 @@
 import React, { ActivityIndicatorIOS, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import commonStyles from './mainComponentStyles';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  progressBar: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  button: {
-    width: 80,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue',
-  },
-  text: {
-    color: 'white',
-  },
-});
+const styles = StyleSheet.create(commonStyles);
 
 const MainComponent = ({ onFetch, isFetching }) => {
   if(isFetching) {
---
+++ src/screens/main/components/mainComponentStyles.js
@@ -0,0 +1,25 @@
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  progressBar: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  button: {
+    width: 80,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+  },
+  text: {
+    color: 'white',
+  },
+};
+
+export default styles;
